f0bb3b34c9df0cf8e9505f9698ef17d179948aef
DESKTOP-UUPP9C7\user <nikd926@gmail.com>
2022-05-16 13:06:49 +0300

    Add Nu-Get packages. Compile css-file from sass-file. Add HomeController and Action "Index"

--- CarComponents/CarComponents/Program.cs
+++ CarComponents/CarComponents/Program.cs
@@ -6,11 +6,9 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
@@ -20,8 +18,9 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.Run();
