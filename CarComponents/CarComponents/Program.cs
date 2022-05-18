e3ca7d327d7d45234efe78db3de98303424157d9
DESKTOP-UUPP9C7\user <nikd926@gmail.com>
2022-05-18 22:38:11 +0300

    Update appsettings.json. Add Congif and bind appsettings.json to Config

--- CarComponents/CarComponents/Program.cs
+++ CarComponents/CarComponents/Program.cs
@@ -1,8 +1,10 @@
+using CarComponents.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Configuration.Bind("Project", new Config());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ CarComponents/CarComponents/Service/Config.cs
@@ -0,0 +1,12 @@
+﻿namespace CarComponents.Service
+{
+    public class Config
+    {
+        public static string? ConnectionString { get; set; }
+        public static string? ProjectName { get; set; }
+        public static string? Phone { get; set; }
+        public static string? PhoneShort { get; set; }
+        public static string? Email { get; set; }
+
+    }
+}
